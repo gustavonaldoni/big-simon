d5de2be9d22fa1d913be2cae3b375a1934d9515c
Gustavo Naldoni <gunaldoni@gmail.com>
2022-12-28 09:11:51 -0300

    Add player struct and basic functions

--- include/player.h
+++ include/player.h
@@ -1,7 +1,10 @@
 typedef struct
 {
     int score;
+    int numberOfPlays;
     List playsList;
 } Player;
 
 void CreatePlayer(Player *);
+int PlayIsValid(Player *, int, int, Board *, int);
+int AddPlayerPlayToList(Player *, int, int, Board *, int);
--- lib/player.c
+++ lib/player.c
@@ -1,5 +1,8 @@
 #include "raylib.h"
 #include "list.h"
+#include "board.h"
+#include "stopwatch.h"
+#include "bot.h"
 #include "player.h"
 
 void CreatePlayer(Player *player)
@@ -8,5 +11,23 @@ void CreatePlayer(Player *player)
     ListCreate(&playsList);
 
     player->score = 0;
+    player->numberOfPlays = 0;
     player->playsList = playsList;
+}
+
+int PlayIsValid(Player *player, int i, int j, Board *board, int roundNumber)
+{
+    return (i >= 0 && i < board->matrixOrder) &&
+           (j >= 0 && j < board->matrixOrder) &&
+           (player->numberOfPlays <= CalculateNumberOfBotPlays(roundNumber));
+}
+
+int AddPlayerPlayToList(Player *player, int i, int j, Board *board, int roundNumber)
+{
+    if (!PlayIsValid(player, i, j, board, roundNumber))
+        return 0;
+
+    ListInsertEnd(&(player->playsList), i, j);
+    player->numberOfPlays += 1;
+    return 1;
 }
